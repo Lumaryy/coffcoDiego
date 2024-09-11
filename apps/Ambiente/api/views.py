45e85ca3ed8f560a69a07f3c603c90efe8f47418
Lumaryy <luisamariasancheztovar2006@gmail.com>
2024-09-11 11:06:52 -0500

    cambios casita

--- apps/Ambiente/api/views.py
+++ apps/Ambiente/api/views.py
@@ -1,45 +1,43 @@
-from rest_framework import viewsets, status
+from rest_framework import generics, status
 from rest_framework.response import Response
 from apps.Ambiente.models import Ambiente
 from apps.Ambiente.api.Serializer import AmbienteSerializer
 
-class AmbienteViewSet(viewsets.ViewSet):
+# aqui listo todos mis ambientes con (list) y puedo agregar uno nuevo con (create)
+class ListarCrearAmbientes(generics.ListCreateAPIView):
+    queryset = Ambiente.objects.all()
+    serializer_class = AmbienteSerializer
 
     def list(self, request):
-        ambientes = Ambiente.objects.all()
-        serializer = AmbienteSerializer(ambientes, many=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
-
-    def retrieve(self, request, pk=None):
-        try:
-            ambiente = Ambiente.objects.get(pk=pk)
-        except Ambiente.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = AmbienteSerializer(ambiente)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
+        queryset = self.get_queryset()
+        if queryset.exists():
+            serializer = self.get_serializer(queryset, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"message": "No hay ambientes registrados"}, status=status.HTTP_404_NOT_FOUND)
 
     def create(self, request):
-        serializer = AmbienteSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED, data=serializer.data)
-        return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+        return super().create(request)
 
-    def update(self, request, pk=None):
-        try:
-            ambiente = Ambiente.objects.get(pk=pk)
-        except Ambiente.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        serializer = AmbienteSerializer(ambiente, data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            serializer.save()
-            return Response(status=status.HTTP_200_OK, data=serializer.data)
-        return Response(status=status.HTTP_400_BAD_REQUEST, data=serializer.errors)
+# y aqui el resto de funciones que requieren id
+class DetalleActualizarEliminarAmbiente(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Ambiente.objects.all()
+    serializer_class = AmbienteSerializer
+    lookup_field = 'id'
 
-    def destroy(self, request, pk=None):
+    def retrieve(self, request, *args, **kwargs):
         try:
-            ambiente = Ambiente.objects.get(pk=pk)
+            return super().retrieve(request, *args, **kwargs)
         except Ambiente.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        ambiente.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response({"message": "Ambiente no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+    def update(self, request, *args, **kwargs):
+        try:
+            return super().update(request, *args, **kwargs)
+        except Ambiente.DoesNotExist:
+            return Response({"message": "Ambiente no encontrado"}, status=status.HTTP_404_NOT_FOUND)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super().destroy(request, *args, **kwargs)
+        except Ambiente.DoesNotExist:
+            return Response({"message": "Ambiente no encontrado"}, status=status.HTTP_404_NOT_FOUND)
